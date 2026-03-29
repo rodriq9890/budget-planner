d5b3b98535c074b720fd14083f3ec249ac330188
McLunar Project <rodri@RODRIGO_PC>
2026-03-29 16:24:37 -0400

    move firebase config to env variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -12,12 +12,12 @@ import {
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYTzJcW-np6LlWGYpIklBM6BrQTxCVDHo",
-  authDomain: "budget-planner-9890.firebaseapp.com",
-  projectId: "budget-planner-9890",
-  storageBucket: "budget-planner-9890.firebasestorage.app",
-  messagingSenderId: "1037871496243",
-  appId: "1:1037871496243:web:a872e4d29212ea5ab4f746",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
